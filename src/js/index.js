c778a9de48faaf58cb0ae6302aa34e2ce8c9190a
CodiguitoJR04 <reyes200402@gmail.com>
2025-10-14 19:16:06 -0600

    pagina-prueba-inicio

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document is ready!');
+    // Aquí puedes agregar más código JavaScript para inicializar tu proyecto
+});
